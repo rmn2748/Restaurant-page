556b5c33324110b6fa9c5cd3a25a8e7fa55333e2
Romain <romain.wojciechowski@gmail.com>
2022-10-09 19:40:40 +0200

    webpack config with watch, htmlplugin, netc..

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+    mode: 'development',
   entry: './src/index.js',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Restaurant Page',
     }),
   ],
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
